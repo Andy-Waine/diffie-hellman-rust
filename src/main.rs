526099d07ef98995cfdf09857e5d95a86de36643
Andy Waine <88730354+Andy-Waine@users.noreply.github.com>
2023-04-25 14:48:20 -0400

    random number generator and prime-checker

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,52 @@
+use rand::Rng;
 
 // NEED: a main function to call sub-functions and retain variables/state for easy re-use
 fn main() {
     println!("Howdy Partner");
 }
 
-// NEED: a function to generate a random prime number (N)
+/*
+    desc    : a function that generates a random prime number (N)
+    params  : <none>
+    returns : u32 - a random prime number between 2 & 10,000
+*/
+fn gen_rand_prime() -> u32 {
+
+    // generates random prime number between 2 & 10,000 
+    let mut rand_num = rand::thread_rng().gen_range(2, 10001);
+
+    // loops until a prime is found, calls is_prime() with each loop
+    while !is_prime(rand_num) {
+        rand_num = rand::thread_rng().gen_range(2, 10001);
+    }
+
+    rand_num
+}
+
+/*
+    desc    : a function checks whether a number is a prime
+    params  : u32 - num
+    returns : bool - true if prime
+*/
+fn is_prime(num: u32) -> bool {
+    
+    if num % 2 == 0 {
+        return false;     //if even, return false
+    }
+    
+    
+    for i in 3.. {                                        // check if the param num is divisible by any odd number between 3 and num.sqrt()
+        if i > (num as f32).sqrt() as u32.step_by(2) {   //step_by(2) double-increments i to skip even numbers
+            break;                                       // once i reaches the square root of num, stop incrementing
+        }
+        if num % i == 0 {
+            return false;
+        }
+    }
+
+    true     // if i reaches the square root of num and no other conditions were met, num is prime
+}
+
 
 // NEED: a function to produce a generator (g) of mod N
 
