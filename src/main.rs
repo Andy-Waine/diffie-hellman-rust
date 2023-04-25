2f8890a7e688a74dbd6489881ceb10239bf9f947
Andy Waine <88730354+Andy-Waine@users.noreply.github.com>
2023-04-25 13:52:33 -0400

    pseudocode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+
+// NEED: a main function to call sub-functions and retain variables/state for easy re-use
 fn main() {
-    println!("Hello, world!");
+    println!("Howdy Partner");
 }
+
+// NEED: a function to generate a random prime number (N)
+
+// NEED: a function to produce a generator (g) of mod N
+
+// NEED: For Alice, a function that...
+    // generates a random private key (Xa) (number less than N, which is coprime by nature)
+    // returns a public key (Ya) equal to (g^(Xa)) mod N
+
+// NEED: For Bob, a function that...
+    // generates a random private key (Xb) (number less than N, which is coprime by nature)
+    // returns a public key (Yb) equal to (g^(Xb)) mod N
+
+// NEED: For Alice, a function that...
+    // accepts Bob's public key (Yb)
+    // returns (Yb^(Xa)) mod N, which is effectively (g^(Xb * Xa)) mod N
+
+// NEED: For Bob, a function that...
+    // accepts Alice's public key (Ya)
+    // returns (Ya^(Xb)) mod N, which is effectively (g^(Xa * Xb)) mod N
+
+// NEED: a function to evaluate whether Alice and Bob's values are equal.
